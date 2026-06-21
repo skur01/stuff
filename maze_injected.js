d570082c47ba2aae6984f202f33bc01d12201034
skur01 <gehrigschreiber@gmail.com>
2026-06-21 08:16:47 -0400

    Refactor maze spawn logic and state management

--- maze_injected.js
+++ maze_injected.js
@@ -411,9 +411,9 @@ game => {
 		const spawnY = (ORIGIN_Y + pointA[1] + 1) * TILE_SIZE;
 		game.map.spawns = game.map.spawns || {};
 		game.map.spawns[0] = [spawnX, spawnY, 1];
-		if (game.render && mazeVar("MazeSpawned", 0) != seed) {
-			game.player.setPosition(spawnX, spawnY);
-			game.trigger("ev[MazeSpawned]=" + seed);
+		if (!game.map.mapVars.MazeSpawned) {
+			if (game.render) game.player.setPosition(spawnX, spawnY);
+			game.map.mapVars.MazeSpawned = 1;
 		}
 		console.log("[MAZE] spawnA set | spawns[0]=" + JSON.stringify(game.map.spawns[0]) + " | candidates=" + doorCandidates.length + " | render=" + game.render + " | player=" + game.player.x + "," + game.player.y);
 	} else {
